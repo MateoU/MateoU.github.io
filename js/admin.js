b4a947bb426e8522a1685f74ac3d24f1b49b1c92
MateoU <matchupicachu@gmail.com>
2016-04-14 12:53:24 -0700

    ...
    ...

--- js/admin.js
+++ js/admin.js
@@ -29,7 +29,13 @@ $(function () {
         var addBlogTemplate = Handlebars.compile(addBlogScript);
         
         $('.main-container').html(addBlogTemplate);
-         tinymce.init({selector:'textarea'});
+         tinymce.init({selector:'textarea',plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});});
     });
     $(document).on('submit', '.form-add-blog', function(event){
        event.preventDefault();
